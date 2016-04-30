202b25335e5e4065e5ed3949ca71a36c5917e037
BrianTParker <brithom.parker@gmail.com>
2016-04-30 15:14:50 -0400

    updated user password column

--- db/schema.rb
+++ db/schema.rb
@@ -30,10 +30,10 @@ ActiveRecord::Schema.define(version: 20160430181735) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",      :limit=>255
-    t.string   "password",   :limit=>255
-    t.datetime "created_at", :null=>false
-    t.datetime "updated_at", :null=>false
+    t.string   "email",           :limit=>255
+    t.string   "password_digest", :limit=>255
+    t.datetime "created_at",      :null=>false
+    t.datetime "updated_at",      :null=>false
   end
 
 end
